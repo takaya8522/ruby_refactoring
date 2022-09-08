92e733ed6b5fae45d725f838b694810885772603
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-09-08 22:49:22 +0900

    ファイルを追加

---
+++ sample.rb
@@ -0,0 +1,47 @@
+def recording_number
+  puts "1から5で評価を入力してください"
+  point = gets.to_i
+  while true
+    if point <= 0 || point > 5
+      puts "1から5で入力してください"
+      point = gets.to_i
+    else
+      puts "コメントを入力してください"
+      comment = gets
+      post = "ポイント：#{point}　コメント：#{comment}"
+      File.open("data.txt", "a") do |file|
+        file.puts(post)
+      end
+      break
+    end
+end
+end
+
+def view_recorded_number
+  puts "これまでの結果"
+  File.open("data.txt", "r") do |file|
+    file.each_line do |line|
+      puts line
+    end
+  end
+end
+
+while true
+  puts "実施したい処理を選択してください"
+  puts "1:評価ポイントとコメントを入力する"
+  puts "2:今までの結果を確認する"
+  puts "3:やめる"
+  num = gets.to_i
+
+  case num
+  when 1
+    recording_number
+  when 2
+    view_recorded_number
+  when 3
+    puts "終了します"
+    break
+  else
+    puts "1から3で入力してください"
+  end
+end
